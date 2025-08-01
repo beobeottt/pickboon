f4ad09012d35b5ec08e01aba38088a511c53d455
long <baolongp9@gmail.com>
2025-08-01 16:51:17 +0700

    veric

--- pick/src/pages/ClubListPage.tsx
+++ pick/src/pages/ClubListPage.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import axiosInstance from "../api/axios";
-
 import { Club } from "../types/Club";
 import Navbar from './NavBar';
 import Footer from "./footer";
